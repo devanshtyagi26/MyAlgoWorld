18066e07436f50eccf8fbe6969a17eda03114198
devanshtyagi26 <tyagidevansh3@gmail.com>
2024-05-16 13:48:44 +0530

    Version 1.1

--- Merge_Sort.cpp
+++ Merge_Sort.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void merge(int A[], int B[], int C[], int m, int n){
     int i,j,k;
     i = j = k = 0;
+    int comparison = 0;
 
     while(i<m && j<n){
         if(A[i] < B[j]){
@@ -16,18 +17,30 @@ void merge(int A[], int B[], int C[], int m, int n){
             j++;
             k++;
         }
+        comparison++;
     }
 
     while(i<m){
         C[k] = A[i];
         i++;
         k++;
+        comparison++;
     }
     while(j<n){
         C[k] = B[j];
         j++;
         k++;
+        comparison++;
     }
+
+    cout << "Merged array: ";
+    cout << "[";
+    for(int i = 0; i<(m+n); i++){
+        cout << C[i]<< " ";
+    }
+    cout <<"]";
+
+    cout << "\n\nNumber of comparisons are: " << comparison;
 }
 
 void singleArrayMerge(int A[], int B[], int start, int mid, int end){
@@ -42,6 +55,16 @@ void singleArrayMerge(int A[], int B[], int start, int mid, int end){
 }
 
 int main(){
+    int A[] = {1, 3, 5, 7};
+    int B[] = {2, 4, 6, 8};
+    int m = sizeof(A) / sizeof(A[0]);
+    int n = sizeof(B) / sizeof(B[0]);
+    int C[m + n];
+
+    merge(A, B, C, m, n);
+
     
+    cout << endl;
+
     return 0;
 }
